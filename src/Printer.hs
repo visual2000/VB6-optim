3196e60dae7ae2e928d4d495692b9890b663d8b9
Paul <423357+toothbrush@users.noreply.github.com>
2020-07-30 18:23:46 +1000

    Print options

--- src/Printer.hs
+++ src/Printer.hs
@@ -10,10 +10,16 @@ class Printable a where
   pp :: a -> Doc
 
 instance Printable Module where
-  pp (Mod as os udts ss) = pp as
+  pp (Mod as os udts ss) = pp as <>
+                           pp os
 
 instance Printable [Attribute] where
   pp [] = text "\n"
   pp ((Attribute n lit) : as) = text "Attribute" <+> text n <+>
                                   char '=' <+> text (show lit)
                                   $+$ pp as
+
+instance Printable [Option] where
+  pp [] = text "\n"
+  pp (OptionExplicit : os) = text "Option Explicit"
+                               $+$ pp os
