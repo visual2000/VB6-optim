d2571ab7436dd7e12ccf9de74c887ef6ba1f2f44
Paul <423357+toothbrush@users.noreply.github.com>
2020-07-30 23:11:26 +1000

    start pretty printing Functions

--- src/Printer.hs
+++ src/Printer.hs
@@ -10,13 +10,14 @@ class Printable a where
   pp :: a -> Doc
 
 instance Printable Module where
-  pp (Mod as os udts ss) =  foldr ($+$) empty
+  pp (Mod as os udts funcs) =  foldr ($+$) empty
                             [ pp as
                             , blank
                             , pp os
                             , blank
                             , pp udts
                             , blank
+                            , pp funcs
                             , text "' The end"
                             ]
 
@@ -24,29 +25,33 @@ blank = text "' ---"
 
 instance Printable [Attribute] where
   pp [] = empty
-  pp ((Attribute n lit) : as) = text "Attribute" <+> text n <+>
-                                  equals <+> pp lit
-                                  $+$ pp as
+  pp ((Attribute n lit) : as) = text "Attribute" <+> text n <+> equals <+> pp lit
+                                $+$ pp as
 
 instance Printable [Option] where
   pp [] = empty
   pp (OptionExplicit : os) = text "Option Explicit"
-                               $+$ pp os
+                             $+$ pp os
 
 
 instance Printable [TypeDef] where
   pp [] = empty
   pp ((TypeDef v n fs) : ts) = (text (show v) <+> text "Type" <+> text n)
-                               $+$ nest 4 (pp fs)
+                               $+$ nest 4 (vcat (map pp fs))
                                $+$ text "End Type"
                                $+$ pp ts
 
-instance Printable [TypeField] where
+instance Printable TypeField where
+  pp (TypeField n ref) = text n <+> text "As" <+> pp ref
+
+instance Printable [FuncDecl] where
   pp [] = empty
-  pp ((TypeField n ref) : tfs) = text n <+>
-                                 text "As" <+>
-                                 pp ref $+$
-                                 pp tfs
+  pp ((FuncDecl v n args ty ss) : fs)
+    = text (show v) <+> text "Function" <+> text n
+      <> parens (hcat $ punctuate (text ", ") (map pp args))
+      <+> text "As" <+> pp ty
+      $+$ text "End Function"
+      $+$ pp fs
 
 instance Printable Lit where
   pp (LInt i) = int i
