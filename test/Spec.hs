23eefe0818f921c52e5e667f481d282491478740
Paul <423357+toothbrush@users.noreply.github.com>
2020-08-01 12:20:53 +1000

    generalise test-AST thing

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,19 +9,20 @@ main = hspec $ do
   describe "Module1 end-to-end" $ do
     it "should parse and print the same module contents" $ do
       contents <- readFile "examples/Module1.bas"
-      test1Output <- test1
+      test1Output <- parseAndPrettyPrintFile "examples/Module1.bas"
       test1Output `shouldBe` contents
 
 
-test1 :: IO String
-test1 = do contents <- readFile "examples/Module1.bas"
-           let toks = parseTokens contents
-           case toks of
-             Left err -> error "halp"
-             Right toks' -> do
-               let mod = parseModule toks'
-               case mod of
-                 Left err -> error "Halp"
-                 Right ast -> do
-                   let result = printModule ast
-                   return result
+parseAndPrettyPrintFile :: FilePath -> IO String
+parseAndPrettyPrintFile f = do
+  contents <- readFile f
+  let toks = parseTokens contents
+  case toks of
+    Left err -> error "halp"
+    Right toks' -> do
+      let mod = parseModule toks'
+      case mod of
+        Left err -> error "Halp"
+        Right ast -> do
+          let result = printModule ast
+          return result
