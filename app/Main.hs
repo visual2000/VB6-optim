21db80b1c60f1634e6875ad3b4d1926c082f40e5
Paul <423357+toothbrush@users.noreply.github.com>
2020-08-01 12:51:12 +1000

    Can read a project file (something.vbp)!

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,9 @@ import Control.Monad.Trans
 import Control.Monad.Except
 import System.Console.Haskeline
 
+import Data.Ini
+import Data.Text (pack, unpack)
+
 import System.IO
 
 import Text.PrettyPrint
@@ -32,6 +35,8 @@ process input = do
           -- putStrLn ("Syntax: " ++ show ast)
           putStrLn $ printModule ast
 
+projectFile = "/Users/paul/Public/BasicTrace/BasicTrace.vbp"
+
 files = [ "examples/HitFuncs.bas"
         , "examples/CameraFuncs.bas"
         , "examples/GfxInit.bas"
@@ -49,5 +54,19 @@ parseFile f = do fileContents <- readFile f
                  mdl <- process fileContents
                  return ()
 
-main :: IO ()
-main = mapM_ parseFile files
+parseProject :: FilePath -> IO (Maybe Ini, [FilePath])
+parseProject p = do hPutStrLn stderr $ "Reading project " ++ p ++ "..."
+                    fileContents <- readIniFile p
+                    case fileContents of
+                      Left err -> do hPutStrLn stderr "Couldn't parse project file!"
+                                     return (Nothing, [])
+                      Right ini -> do
+                        let glo = iniGlobals ini
+                        let mods = map (unpack . snd) $ filter (\(k,v)->(unpack k)=="Module") glo
+                        let stripped = map ((\ws->ws!!1) . words) mods
+                        putStrLn (stripped >>= (\m -> "Found module: " ++ m ++ ".\n"))
+                        return (Just ini, stripped)
+
+main = parseProject projectFile
+-- main :: IO ()
+-- main = mapM_ parseFile files
